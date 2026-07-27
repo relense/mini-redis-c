ec1286c68484cf6519d2d002677a72d448eae1b2
Miguel <miguelmofurtado@gmail.com>
2026-07-27 16:54:10 +0100

    Add check to get the number of arguments from the cmd string

--- src/resp-protocol.c
+++ src/resp-protocol.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include "byte-buffer.h"
 
 parsed_cmd* parse_cmd(char* buffer, size_t buffer_len) {
     if(buffer) {
@@ -17,9 +19,36 @@ parsed_cmd* parse_cmd(char* buffer, size_t buffer_len) {
             .status = PARSE_INCOMPLETE
         };
 
-        for(size_t i = 0; i < buffer_len; i++) {
-            if(!strcmp(&buffer[0],"*")) {
+        size_t i;
+        bool checking_number_args = false;
+        byte_buffer temp_buffer;
+        byte_buffer_init(&temp_buffer, 0);
+        bool checking_arg = false;
+        size_t current_arg = 0;
+
+        //loop to find number of args
+        for(i = 0; i < buffer_len; i++) {
+            if(buffer[i] == '*') {
+                checking_number_args = true;
                 continue;
+            }
+
+            if(buffer[i] == '$') {
+                checking_arg = true;
+                continue;
+            }
+
+            //getting the number of arguments
+            if(checking_number_args && buffer[i] != '\n' && buffer[i] != '\r') {
+                byte_buffer_append(&temp_buffer, &buffer[i], 1);
+            } else if (checking_number_args && buffer[i] == '\n') {
+                for(size_t j = 0; j < temp_buffer.len; j++) {
+                    char* endptr;
+                    unsigned long num = strtoul(temp_buffer.data, &endptr, 10);
+                    cmd->argc = num;
+                    byte_buffer_destroy(&temp_buffer);
+                }
+                checking_number_args = false;
             }
         }
 
