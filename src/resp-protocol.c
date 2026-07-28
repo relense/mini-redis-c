8b03e61fd0c976718fe6eb1a8440277b108d7b45
Miguel <miguelmofurtado@gmail.com>
2026-07-28 11:26:00 +0100

    Add more logic to parse the command WIP

--- src/resp-protocol.c
+++ src/resp-protocol.c
@@ -11,84 +11,121 @@ parsed_cmd* parse_cmd(char* buffer, size_t buffer_len) {
         if(!cmd) return NULL;
 
         *cmd = (parsed_cmd) {
-            .arg_lengths = NULL,
-            .argc = 0,
-            .buffer = NULL,
-            .bytes_consumed = 0,
-            .cmd_name = NULL,
             .status = PARSE_INCOMPLETE
         };
 
         size_t i;
         bool checking_number_args = false;
+        bool checking_arg = false;
         byte_buffer temp_buffer;
         byte_buffer_init(&temp_buffer, 0);
-        bool checking_arg = false;
         size_t current_arg = 0;
         size_t new_line_count = 0;
 
-        //loop to find number of args
         for(i = 0; i < buffer_len; i++) {
+            // if we are getting the number of argc
             if(buffer[i] == '*') {
                 checking_number_args = true;
                 continue;
             }
 
+            // if we are checking an argument
             if(buffer[i] == '$') {
                 checking_arg = true;
                 continue;
             }
 
-            //getting the number of arguments
+            // getting the number of arguments
             if(checking_number_args) {
                 if(buffer[i] != '\n' && buffer[i] != '\r') {
                     byte_buffer_append(&temp_buffer, &buffer[i], 1);
                 } else if (buffer[i] == '\n') {
-                    for(size_t j = 0; j < temp_buffer.len; j++) {
-                        char* endptr;
-                        unsigned long num = strtoul(temp_buffer.data, &endptr, 10);
-                        cmd->argc = num;
-                        byte_buffer_reset(&temp_buffer);
+                    char terminator = '\0';
+                    byte_buffer_append(&temp_buffer, &terminator, 1);
+                    char* endptr;
+                    unsigned long num = strtoul(temp_buffer.data, &endptr, 10);
+                    cmd->argc = num - 1; 
+
+                    cmd->buffer = malloc((num) * sizeof(char*));
+                    if(!cmd->buffer){
+                        free_parsed_cmd(cmd);
+                        byte_buffer_destroy(&temp_buffer);
+                        return NULL;
                     }
+
+                    cmd->arg_lengths = malloc((num) * sizeof(unsigned long));
+                    if(!cmd->arg_lengths) {
+                        free_parsed_cmd(cmd);
+                        byte_buffer_destroy(&temp_buffer);
+                        return NULL;
+                    }
+
                     checking_number_args = false;
+                    byte_buffer_reset(&temp_buffer);
                 }
             }
 
+            //getting information for an argument byte and string
             if(checking_arg) {
                 if(buffer[i] == '\n') { 
                     // if its the last arg, its the end of the resp parsing so save the bytes consumed
-                    if(current_arg == cmd->argc - 1) {
-                        cmd->bytes_consumed = temp_buffer.len;
+                    if(current_arg == cmd->argc) {
+                        cmd->bytes_consumed = buffer_len;
                     }
 
-                    //means we have a cmd to save
-                    if(current_arg == 0 && new_line_count > 0) {
-                        for(size_t j = 0; j < temp_buffer.len; j++) {
-                            cmd->cmd_name[j] = temp_buffer.data[j];
-                        }    
-                    }
-
-                    // means we have an arg that is not a byte and that is not a cmd to save
-                    if (current_arg > 0 && new_line_count > 0) { 
-                         for(size_t j = 0; j < temp_buffer.len; j++) {
-                            cmd->buffer[j] = &temp_buffer.data[j];
+                    // if we have a cmd to save, save
+                    // Should only pass here once
+                    if(current_arg == 0) {
+                        if(new_line_count == 0) {
+                            new_line_count++;
+                            byte_buffer_reset(&temp_buffer);
+                            continue;
                         }
+
+                        cmd->cmd_name = malloc(temp_buffer.len + 1);
+                        if(!cmd->cmd_name) {
+                            free_parsed_cmd(cmd);
+                            byte_buffer_destroy(&temp_buffer);
+                            return NULL;
+                        }
+
+                        memcpy(cmd->cmd_name, temp_buffer.data, temp_buffer.len);
+                        cmd->cmd_name[temp_buffer.len] = '\0';
+
+                        new_line_count = 0;
+                        current_arg++;
+                        byte_buffer_reset(&temp_buffer);
+                        continue;
                     }
 
                     // means we have the bytes for the current arg we are parsing
-                    if(new_line_count == 0) { 
-                            char* endptr;
-                            unsigned long num = strtoul(temp_buffer.data, &endptr, 10);
-                            cmd->arg_lengths[current_arg] = num;
+                    if(current_arg > 0 && new_line_count == 0) {
+                        char terminator = '\0';
+                        byte_buffer_append(&temp_buffer, &terminator, 1);
+                        char* endptr;
+                        unsigned long current_num = strtoul(temp_buffer.data, &endptr, 10);
+                        cmd->arg_lengths[current_arg - 1] = current_num;
+
                         new_line_count++;
+                        byte_buffer_reset(&temp_buffer);
+                        continue;
                     }
 
-                    if(new_line_count > 0) { //means it finished the arg parsing and we reset the new line count and set wich argument we are parsing to the next
+                    // means we have an arg that is not a byte and that is not a cmd to save
+                    if (current_arg > 0 && new_line_count == 1) { 
+                        cmd->buffer[current_arg - 1] = malloc(temp_buffer.len);
+                        if(!cmd->buffer[current_arg - 1]) { 
+                            free_parsed_cmd(cmd);
+                            byte_buffer_destroy(&temp_buffer);
+                            return NULL;
+                        }
+                        memcpy(cmd->buffer[current_arg - 1], temp_buffer.data, temp_buffer.len);
+
                         new_line_count = 0;
                         current_arg++;
+                        byte_buffer_reset(&temp_buffer);
+                        continue;
                     }
-
-                    byte_buffer_reset(&temp_buffer);
                 }
 
                 if(buffer[i] != '\n' && buffer[i] != '\r') {
@@ -97,16 +134,20 @@ parsed_cmd* parse_cmd(char* buffer, size_t buffer_len) {
             }
         }
 
+        if(current_arg == cmd->argc + 1) {
+            cmd->status = PARSE_COMPLETE;
+        }
+
+        byte_buffer_destroy(&temp_buffer);
+
+        
         for(size_t k = 0; k < cmd->argc; k++) {
+            printf("CURRENT K %zu\n", k);
+            printf("ARG VALUE %lu\n", cmd->arg_lengths[k]);
             for(size_t t = 0; t < cmd->arg_lengths[k]; t++) {
-                if(cmd->buffer[k][t] == '\r') {
-                    printf("\\r");
-                } else if (cmd->buffer[k][t] == '\n') {
-                    printf("\\n");
-                } else {
-                    printf("%c", cmd->buffer[k][t]);
-                }    
+                printf("%c", cmd->buffer[k][t]);
             } 
+             printf("\n");
         }
 
         printf("\n");
@@ -119,8 +160,25 @@ parsed_cmd* parse_cmd(char* buffer, size_t buffer_len) {
 
 void free_parsed_cmd(parsed_cmd* cmd) {
     if(cmd) {
+        if(cmd->buffer) {
+            for(size_t i = 0; i < cmd->argc; i++) {
+                if(cmd->buffer[i]) free(cmd->buffer[i]);
+            }
+
+            free(cmd->buffer);
+        }
+
+        if(cmd->arg_lengths) {
+            free(cmd->arg_lengths);
+        }
+
+        if(cmd->cmd_name) {
+            free(cmd->cmd_name);
+        }
+
+        *cmd = (parsed_cmd) { };
         free(cmd);
-    }
+    } 
 }
 
 // encoded_resp* encode_resp(cmd_result* cmd) {
