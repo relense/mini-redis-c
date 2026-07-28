a8d5cc96a12b95d3892fbe493424da4117d14dbd
Miguel <miguelmofurtado@gmail.com>
2026-07-28 14:20:03 +0100

    Refactor the parse_cmd function by passing the getting number of args section to a function

--- src/resp-protocol.c
+++ src/resp-protocol.c
@@ -5,6 +5,39 @@
 #include <stdbool.h>
 #include "byte-buffer.h"
 
+static bool get_number_of_args(bool* checking_number_args, const char* buffer, const size_t index, byte_buffer* temp_buffer, parsed_cmd* cmd ) {
+    if(*checking_number_args) {
+        if(buffer[index] != '\n' && buffer[index] != '\r') {
+            byte_buffer_append(temp_buffer, &buffer[index], 1);
+        } else if (buffer[index] == '\n') {
+            char terminator = '\0';
+            byte_buffer_append(temp_buffer, &terminator, 1);
+            char* endptr;
+            unsigned long num = strtoul(temp_buffer->data, &endptr, 10);
+            cmd->argc = num - 1; 
+
+            cmd->buffer = malloc((num) * sizeof(char*));
+            if(!cmd->buffer){
+                free_parsed_cmd(cmd);
+                byte_buffer_destroy(temp_buffer);
+                return false;
+            }
+
+            cmd->arg_lengths = malloc((num) * sizeof(unsigned long));
+            if(!cmd->arg_lengths) {
+                free_parsed_cmd(cmd);
+                byte_buffer_destroy(temp_buffer);
+                return false;
+            }
+
+            *checking_number_args = false;
+            byte_buffer_reset(temp_buffer);
+        }
+    }
+
+    return true;
+}
+
 parsed_cmd* parse_cmd(char* buffer, size_t buffer_len) {
     if(buffer) {
         parsed_cmd* cmd = malloc(sizeof(parsed_cmd));
@@ -36,34 +69,7 @@ parsed_cmd* parse_cmd(char* buffer, size_t buffer_len) {
             }
 
             // getting the number of arguments
-            if(checking_number_args) {
-                if(buffer[i] != '\n' && buffer[i] != '\r') {
-                    byte_buffer_append(&temp_buffer, &buffer[i], 1);
-                } else if (buffer[i] == '\n') {
-                    char terminator = '\0';
-                    byte_buffer_append(&temp_buffer, &terminator, 1);
-                    char* endptr;
-                    unsigned long num = strtoul(temp_buffer.data, &endptr, 10);
-                    cmd->argc = num - 1; 
-
-                    cmd->buffer = malloc((num) * sizeof(char*));
-                    if(!cmd->buffer){
-                        free_parsed_cmd(cmd);
-                        byte_buffer_destroy(&temp_buffer);
-                        return NULL;
-                    }
-
-                    cmd->arg_lengths = malloc((num) * sizeof(unsigned long));
-                    if(!cmd->arg_lengths) {
-                        free_parsed_cmd(cmd);
-                        byte_buffer_destroy(&temp_buffer);
-                        return NULL;
-                    }
-
-                    checking_number_args = false;
-                    byte_buffer_reset(&temp_buffer);
-                }
-            }
+            if(!get_number_of_args(&checking_number_args, buffer, i, &temp_buffer, cmd)) return NULL;
 
             //getting information for an argument byte and string
             if(checking_arg) {
