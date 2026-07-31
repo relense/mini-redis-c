df7afa16fd0b119b0e794a3499dff57ad3463979
Miguel <miguelmofurtado@gmail.com>
2026-07-31 15:06:53 +0100

    Correct missing signature attribute

--- src/hash-map.h
+++ src/hash-map.h
@@ -18,7 +18,7 @@ typedef struct {
     entry** buckets;
 } hash_map;
 
-hash_map* hash_map_init(hash_map* map);
+hash_map* hash_map_init(hash_map* map, size_t cap);
 void hash_map_destroy(hash_map* map);
 
 hash_map* hash_map_put(hash_map* map, char* key, char* value);
