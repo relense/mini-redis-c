0cc8d04d90a792da274d57c18a12d8f5756393f2
Miguel <miguelmofurtado@gmail.com>
2026-08-01 17:41:09 +0100

    Add checks to be sure that we throw a system_error when memory allocation fails

--- src/commands.c
+++ src/commands.c
@@ -13,6 +13,7 @@ typedef enum {
 
 typedef enum {
     TOO_MANY_ARGS,
+    NOT_ENOUGH_ARGS,
     CMD_UNKNOWN,
 } error_types;
 
@@ -34,12 +35,32 @@ static cmd_result build_error_response(char* cmd_name, error_types error_type) {
         snprintf(message, sizeof(message), "%s: unknown command", cmd_name);
     } else if (error_type == TOO_MANY_ARGS) {
         snprintf(message, sizeof(message), "%s: too many args", cmd_name);
+    } else if (error_type == TOO_MANY_ARGS) {
+        snprintf(message, sizeof(message), "%s: not enough args", cmd_name);
     }
 
+    char* copied_message = strdup(message);
+    if(!copied_message) return build_system_error_response();
+
     result = (cmd_result) {
-        .result = strdup(message),
-        .result_len = strlen(message),
+        .result = copied_message,
+        .result_len = strlen(copied_message),
         .status = SIMPLE_ERROR
+    };
+
+    return result;
+}
+
+static cmd_result build_bulk_string(char* message) {
+    cmd_result result;
+
+    char* copied_message = strdup(message);
+    if(!copied_message) return build_system_error_response();
+
+    result = (cmd_result) {
+        .result = copied_message,
+        .result_len = strlen(copied_message),
+        .status = BULK_STRING
     };
 
     return result;
@@ -48,9 +69,12 @@ static cmd_result build_error_response(char* cmd_name, error_types error_type) {
 static cmd_result build_simple_string(char* message) {
     cmd_result result;
 
+    char* copied_message = strdup(message);
+    if(!copied_message) return build_system_error_response();
+
     result = (cmd_result) {
-        .result = strdup(message),
-        .result_len = strlen(message),
+        .result = copied_message,
+        .result_len = strlen(copied_message),
         .status = SIMPLE_STRING
     };
 
@@ -59,9 +83,10 @@ static cmd_result build_simple_string(char* message) {
 
 static cmd_result build_system_error_response() {
     cmd_result result;
+    char* message = strdup("");
 
     result = (cmd_result) {
-        .result = strdup(""),
+        .result = message,
         .result_len = 0,
         .status = SYSTEM_ERROR
     };
@@ -72,64 +97,75 @@ static cmd_result build_system_error_response() {
 static cmd_result execute_get(char* cmd_name, char** buffer, size_t* arg_lengths, size_t argc) {
     if(argc > 1) {
         return build_error_response(cmd_name, TOO_MANY_ARGS);
-    }
-
-    cmd_result result;
-    char* key = to_null_terminated(buffer[0], arg_lengths[0]);
-    char* get_result = storage_get(key);
-
-    if(!get_result) {
-        result = (cmd_result) {
-            .result = strdup(""),
-            .result_len = 0,
-            .status = BULK_STRING
-        };
+    } else if (argc < 1) {
+        return build_error_response(cmd_name, NOT_ENOUGH_ARGS);
     } else {
-        result = (cmd_result) {
-            .result = strdup(get_result),
-            .result_len = strlen(get_result),
-            .status = BULK_STRING
-        };
-    }
+        cmd_result result;
 
-    free(key);
-    return result;
+        char* key = to_null_terminated(buffer[0], arg_lengths[0]);
+        if(!key) {
+            return build_system_error_response();
+        }
+
+        char* get_result = storage_get(key);
+        free(key);
+
+        if(!get_result) {
+            return build_bulk_string("");
+        } else {
+            return build_bulk_string(get_result);
+        }
+    }
 }
 
 static cmd_result execute_set(char* cmd_name, char** buffer, size_t* arg_lengths, size_t argc) {
-     if(argc > 2) {
+    if(argc > 2) {
         return build_error_response(cmd_name, TOO_MANY_ARGS);
+    } else if (argc < 2) {
+        return build_error_response(cmd_name, NOT_ENOUGH_ARGS);
+    } else {
+        bool system_error = false;
+        char* key = to_null_terminated(buffer[0], arg_lengths[0]);
+        if(!key) {
+            return build_system_error_response();
+        }
+        char* value = to_null_terminated(buffer[1], arg_lengths[1]);
+        if(!value) {
+            return build_system_error_response();
+        }
+
+        storage_set(key, value, &system_error);
+
+        free(key);
+        free(value);
+
+        if(system_error) {
+            return build_system_error_response();
+        }
+
+        char* message = "OK";
+        return build_simple_string(message);
     }
-
-    bool system_error = false;
-    char* key = to_null_terminated(buffer[0], arg_lengths[0]);
-    char* value = to_null_terminated(buffer[1], arg_lengths[1]);
-
-    storage_set(key, value, &system_error);
-
-    free(key);
-    free(value);
-
-    if(system_error) {
-        return build_system_error_response();
-    }
-
-    char* message = "OK";
-    return build_simple_string(message);
 }
 
 static cmd_result execute_del(char* cmd_name, char** buffer, size_t* arg_lengths, size_t argc) {
     if(argc > 1) {
         return build_error_response(cmd_name, TOO_MANY_ARGS);
+    } else if (argc < 1) {
+        return build_error_response(cmd_name, NOT_ENOUGH_ARGS);
+    } else {
+        char* key = to_null_terminated(buffer[0], arg_lengths[0]);
+        if(!key) {
+            return build_system_error_response();
+        }
+    
+        storage_del(key);
+
+        free(key);
+        char* message = "OK";
+
+        return build_simple_string(message);
     }
-
-    char* key = to_null_terminated(buffer[0], arg_lengths[0]);
-    storage_del(key);
-
-    free(key);
-    char* message = "OK";
-
-    return build_simple_string(message);
 }
 
 static cmd_result execute_pong(char* cmd_name, size_t argc) {
