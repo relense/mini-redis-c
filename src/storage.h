bcc51d5dabbb69739bd9f5692f4a72391ff2cead
Miguel <miguelmofurtado@gmail.com>
2026-08-01 20:27:56 +0100

    Update storage_set to also receive value_len to store in hash_map

--- src/storage.h
+++ src/storage.h
@@ -8,7 +8,7 @@ typedef struct {
     size_t len;
 } storage_result;
 
-void storage_set(const char* key, const char* value, bool* system_error);
+void storage_set(const char* key, const char* value, size_t value_len, bool* system_error);
 storage_result storage_get(const char* key);
 void storage_del(const char* key);
 
