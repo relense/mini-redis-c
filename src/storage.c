a2abcc7513230059f3fc17ea8e253c51b29529b0
Miguel <miguelmofurtado@gmail.com>
2026-08-01 16:58:28 +0100

    Update the storage.h and add the empty functions to storage.c

---
+++ src/storage.c
@@ -0,0 +1,14 @@
+#include "storage.h"
+#include <stdbool.h>
+
+void storage_set(const char* key, const char* value, bool* system_error) {
+
+}
+
+char* storage_get(const char* key) {
+
+}
+
+void storage_del(const char* key) {
+
+}
--- src/storage.h
+++ src/storage.h
@@ -1,8 +1,10 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-void storage_set(const char* key, const char* value);
+#include <stdbool.h>
+
+void storage_set(const char* key, const char* value, bool* system_error);
 char* storage_get(const char* key);
-bool storage_del(const char* key);
+void storage_del(const char* key);
 
 #endif
