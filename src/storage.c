60aab47e106e095490fbe0f7a90fb2bf49a8b5ac
Miguel <miguelmofurtado@gmail.com>
2026-08-02 11:38:21 +0100

    Add the storage_set, get and del functions

--- src/storage.c
+++ src/storage.c
@@ -14,13 +14,32 @@ void storage_destroy(void) {
 }
 
 void storage_set(const char* key, const char* value, size_t value_len, bool* system_error) {
-
+    if(!hash_map_put(&map, key, value, value_len)) {
+        *system_error = true;
+    } else {
+        *system_error = false;
+    }
 }
 
 storage_result storage_get(const char* key) {
+    entry_result found_entry;
 
+    found_entry = hash_map_get(&map, key);
+    if(!found_entry.value) {
+        return (storage_result) {
+            .key_exists = false,
+            .value = NULL,
+            .value_len = 0
+        };
+    } else {
+        return (storage_result) {
+            .key_exists = true,
+            .value = found_entry.value,
+            .value_len = found_entry.value_len
+        };
+    }
 }
 
 void storage_del(const char* key) {
-
+    hash_map_remove(&map, key);
 }
